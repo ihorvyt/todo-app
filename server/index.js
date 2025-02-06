43346bdd70205f18f163974d474bd7a8f03e021c
ihorvyt <ihorvyt@gmail.com>
2025-02-06 12:30:33 +0200

    Initialize backend with Prisma setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => console.log("Server running on port 5000"));
